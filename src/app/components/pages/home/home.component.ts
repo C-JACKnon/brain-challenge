8a4c5ceb184f0a8332e75566339af1271363da42
C-JACKnon <jack.magatcha@gmail.com>
2024-11-30 18:12:06 +0900

    [add] ユーザ名入力欄の実装

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -3,6 +3,7 @@ import { ChangeComponentService } from '../../../core/services/change-component.
 import { PAGE_ADDRESS } from '../../../app.routes';
 import { SquareButtonComponent } from '../../share/square-button/square-button.component';
 import { AngularSvgIconModule } from 'angular-svg-icon';
+import { CommonModule } from '@angular/common';
 
 /**
  * ホーム画面コンポーネント
@@ -12,6 +13,7 @@ import { AngularSvgIconModule } from 'angular-svg-icon';
   selector: 'app-home',
   standalone: true,
   imports: [
+    CommonModule,
     SquareButtonComponent,
     AngularSvgIconModule,
   ],
@@ -19,6 +21,8 @@ import { AngularSvgIconModule } from 'angular-svg-icon';
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  public isFocusUserNameInputForm: boolean = false;
+
   /**
    * @constructor
    * @param changeComponentService 画面コンポーネント切替サービス
@@ -26,6 +30,18 @@ export class HomeComponent {
   constructor(
     private changeComponentService: ChangeComponentService,
   ) { }
+
+  /**
+   * ユーザ名入力欄へのフォーカスイベント
+   * @param isFocus フォーカスされたか否か
+   */
+  public onFocusUserNameInputForm(isFocus: boolean): void {
+    this.isFocusUserNameInputForm = isFocus;
+
+    if (!isFocus) {
+      this.userNameValidation();
+    }
+  }
 
   /**
    * STARTボタンクリックイベント
@@ -46,5 +62,51 @@ export class HomeComponent {
    */
   public onClickHowToPlayButton(): void {
     this.changeComponentService.changePage(PAGE_ADDRESS.HOW_TO);
+  }
+
+  /**
+   * ユーザ名バリデーションチェックを実行
+   * - 先頭と最後の空白を削除する
+   * - 最大文字数内に短くする
+   */
+  private userNameValidation(): void {
+    const userNameInputFormElement = document.getElementById('user-name') as HTMLInputElement;
+    let formatUserName = userNameInputFormElement.value;
+
+    // 先頭の空文字を削除する
+    let emptyIndex = 0;
+    for (let i = 0; i < formatUserName.length; i++) {
+      if (formatUserName[i] === ' ' || formatUserName[i] === '　') {
+        emptyIndex++;
+      }
+      else {
+        break;
+      }
+    }
+    if (emptyIndex > 0) {
+      formatUserName = formatUserName.slice(emptyIndex);
+    }
+
+    // 最後の空文字を削除する
+    emptyIndex = 0;
+    for (let i = formatUserName.length - 1; i >= 0; i--) {
+      if (formatUserName[i] === ' ' || formatUserName[i] === '　') {
+        emptyIndex++;
+      }
+      else {
+        break;
+      }
+    }
+    if (emptyIndex > 0) {
+      formatUserName = formatUserName.slice(0, -emptyIndex);
+    }
+
+    // 最大文字数を超えている場合は最後の文字から超えている文字数分削除する
+    const maxUserNameLength = 15;
+    if (formatUserName.length > maxUserNameLength) {
+      formatUserName = formatUserName.slice(0, maxUserNameLength);
+    }
+
+    userNameInputFormElement.value = formatUserName;
   }
 }
