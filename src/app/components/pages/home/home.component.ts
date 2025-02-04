3722fa2549401144461253d9f994df7947343e1d
C-JACKnon <jack.magatcha@gmail.com>
2025-02-05 06:04:03 +0900

    [Make10] プレリリースで遊び方画面とランキング画面をドロップするための実装

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -47,9 +47,10 @@ export class HomeComponent implements AfterViewInit {
    * ライフサイクル: ビュー表示後
    */
   public ngAfterViewInit(): void {
-    // 登録されているプレイヤー名を設定する
-    const playerNameInputFormElement = document.getElementById('user-name') as HTMLInputElement;
-    playerNameInputFormElement.value = this.playerNameService.playerName;
+    // TODO: プレリリースではランキング画面をドロップするためコメントアウト
+    // // 登録されているプレイヤー名を設定する
+    // const playerNameInputFormElement = document.getElementById('user-name') as HTMLInputElement;
+    // playerNameInputFormElement.value = this.playerNameService.playerName;
   }
 
   /**
